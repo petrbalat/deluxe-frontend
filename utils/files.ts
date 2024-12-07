d6546301fc2b2b8908ec4848ea8d26a8fb4e1a11
Petr Balat <petr.balat@gmail.com>
2024-12-07 17:36:39 +0100

    FileEntry

--- utils/files.ts
+++ utils/files.ts
@@ -1,19 +1,24 @@
 
 export async function* listFiles(directory: string, filter?: {
-  endsWith?: string,
-}): AsyncGenerator<Deno.DirEntry, void, unknown> {
+  endsWith?: string;
+}): AsyncGenerator<FileEntry, void, unknown> {
   for await (const it of Deno.readDir(directory)) {
     if (!it.isFile) {
       continue;
     }
 
+    const entry: FileEntry = { ...it, ...{ fullPath: `${directory}/${it.name}` } };
     if (!filter) {
-      yield it;
+      yield entry;
       continue;
     }
 
     if (!filter.endsWith || it.name.endsWith(filter.endsWith)) {
-      yield it;
+      yield entry;
     }
   }
 }
+
+export type FileEntry = Deno.DirEntry & {
+  fullPath: string;
+};
